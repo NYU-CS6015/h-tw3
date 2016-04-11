677ae175857220de4f4b0783a1679695acfd3249
Monil Suthar <sutharmonil@gmail,com>
2016-04-11 18:51:55 -0400

    First

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Team 3</div>
+                <div class="title">Hello Team 3</div>
             </div>
         </div>
     </body>
